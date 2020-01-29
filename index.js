0f63d9745e0210d3d0969e19c2b7010b447a65bc
rednaxela700 <ohleg@windowslive.com>
2020-01-29 12:05:50 +0100

    added kbd, finished transition roll back

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-window.addEventListener("keydown", (e) => {
+function playSound (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
-    console.log(audio)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    console.log('key', key)
+    console.log(audio);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing")
+}
 
-});
+const keys = document.querySelectorAll('.key');
+
+function removeTransition  (e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove("playing")
+};
+
+keys.forEach(el => el.addEventListener('transitionend', removeTransition));
+
+window.addEventListener("keydown", playSound);
