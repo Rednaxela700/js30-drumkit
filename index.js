0716f95bd903ae31df13a901025a237609b650d6
rednaxela700 <ohleg@windowslive.com>
2020-02-07 18:25:35 +0100

    indes.js: deleted console.logs

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function playSound (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    console.log('key', key);
-    console.log(audio);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
