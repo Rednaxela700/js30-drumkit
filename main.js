5ba1f7d73285f993719235577f890a5f44bf71d1
rednaxela700 <ohleg@windowslive.com>
2020-02-09 22:44:48 +0100

    main.js: added fn checking boxes from target to box no.4

--- main.js
+++ main.js
@@ -5,5 +5,15 @@ console.log(checkboxesArr)
 checkboxesArr.map(
     (el, idx) => el.no = idx)
 
+function checkEmpty(start, end) {
+    end.no
+    console.log(start)
+    const currentCheckboxes = checkboxesArr.filter(item => item.no >= start.no && item.no <= end.no && item.checked !== start.checked)
+    currentCheckboxes.forEach(el => el.checked = start.checked)
+    console.log(currentCheckboxes)
+}
 
-checkboxesArr.forEach(el => el.addEventListener('click', (e) => console.log(e.target.no)))
+checkboxesArr.forEach(el => el.addEventListener('click', (e) => {
+    console.log(e.target.no)
+    checkEmpty(e.target, checkboxesArr[3])
+}))
